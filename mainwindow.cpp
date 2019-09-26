1c2780b063105abeefb4b13f962fbd5608904191
TianSHH <nd2brth@126.com>
2019-09-26 10:23:09 +0800

    完成减采样功能

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -93,52 +93,30 @@ void MainWindow::on_actionSamplingRate_triggered()
     QImage originImage = rightPixmapItem->pixmap().toImage();
     QImage newImage = rightPixmapItem->pixmap().toImage();
 
-    int kernel[5][5] = {{0, 0, 1, 0, 0},
-                        {0, 1, 3, 1, 0},
-                        {1, 3, 7, 3, 1},
-                        {0, 1, 3, 1, 0},
-                        {0, 0, 1, 0, 0}};
+    int samplingRate = 4;
 
-    int kernelSize = 5;
-    int sumKernel = 27;
+    int width = newImage.width();
+    int height = newImage.height();
+
     int r, g, b;
+
     QColor color;
 
-    qDebug() << newImage.width();
-    qDebug() << newImage.height();
-
-    for (int x = kernelSize / 2; x < newImage.width() - (kernelSize / 2); x++)
-    {
-        for (int y = kernelSize / 2; y < newImage.height() - (kernelSize / 2); y++)
+    for (int i = 1; i <= height; i += samplingRate)
+        for (int j = 1; j <= width; j += samplingRate)
         {
+            color = QColor(originImage.pixel(i, j));
+            r = color.red();
+            g = color.green();
+            b = color.blue();
 
-            r = 0;
-            g = 0;
-            b = 0;
+            newImage.setPixel(i + 1, j + 1, qRgb(r, g, b));
 
-            for (int i = -kernelSize / 2; i <= kernelSize / 2; i++)
-            {
-                for (int j = -kernelSize / 2; j <= kernelSize / 2; j++)
-                {
-                    color = QColor(originImage.pixel(x + i, y + j));
-                    r += color.red() * kernel[kernelSize / 2 + i][kernelSize / 2 + j];
-                    g += color.green() * kernel[kernelSize / 2 + i][kernelSize / 2 + j];
-                    b += color.blue() * kernel[kernelSize / 2 + i][kernelSize / 2 + j];
-                }
-            }
-
-            r = qBound(0, r / sumKernel, 255);
-            g = qBound(0, g / sumKernel, 255);
-            b = qBound(0, b / sumKernel, 255);
-
-            newImage.setPixel(x, y, qRgb(r, g, b));
+            for (int p = 0; p < samplingRate && i + p <= height; p++)
+                for (int q = 0; q < samplingRate && j + q <= width; q++)
+                    newImage.setPixel(i + p, j + q, qRgb(r, g, b));
         }
-    }
-    //    rightScene->addPixmap(QPixmap::fromImage(newImage));
 
-    qDebug() << "???";
-
-    //    newImage.load("/home/xhls/Pictures/asuka.jpg");
     updateRightImage(newImage);
 
     qDebug() << "Done!";
