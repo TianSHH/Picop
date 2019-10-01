375624a6889b246e0348fe119b34c6de07f1c0bd
TianSHH <nd2brth@126.com>
2019-10-01 22:07:45 +0800

    初步完成优化

--- dialoghistogram.cpp
+++ dialoghistogram.cpp
@@ -17,7 +17,7 @@ void DialogHistogram::setup()
 
     init();
 
-    this->resize(800, 800);
+    this->setFixedSize(606, 410);
 
     this->setWindowTitle("直方图 - Picop");
 } // setup
@@ -53,12 +53,9 @@ void DialogHistogram::displayHistogram(QImage *originImage)
     qDebug().noquote() << "[Debug]" << QDateTime::currentDateTimeUtc().toString("yyyy-MM-dd hh:mm:ss.zzz") << ":"
                        << "displayHistogram";
 
-    // qDebug().noquote() << "[Debug]" << QDateTime::currentDateTimeUtc().toString("yyyy-MM-dd hh:mm:ss.zzz") << ":"
-    //    << "显示直方图";
-
-    int width = originImage->width();
-    int height = originImage->height();
-    int totalPixel = width * height;
+    width = originImage->width();
+    height = originImage->height();
+    totalPixel = width * height;
 
     if (!originImage->isNull())
     {
@@ -115,317 +112,31 @@ void DialogHistogram::paintEvent(QPaintEvent *event)
 
     Q_UNUSED(event);
 
-    int dist = 100;              // 直方图之间的距离
-    int height = 255 + 10;       // 直方图高度
-    int xBase = 99;              // 第一个直方图原点的 x 坐标
-    int yBase = 30 + height + 1; // 第一个直方图原点的 y 坐标
-
     QPainter painter(this);
     painter.setPen(Qt::black);
 
-    // 灰度直方图
-    if (grayHistogram[256] != -1)
-        drawGrayHistogram(xBase, yBase, height);
-    else
-        painter.drawText(xBase, yBase - height / 2 + 5, tr("Can not load the gray level histogram."));
+    drawHistogram(xBase, yBase, widthHistogram, heightHistogram, grayHistogram);
 
-    // red hstgrm
-    if (redHistogram[256] != -1)
-        drawRedHistogram(xBase + dist + height, yBase, height);
-    else
-        painter.drawText(xBase + dist + height + 1 - height / 2 + 5, yBase, tr("Can't load the red component histogram."));
+    drawHistogram(xBase + dist + widthHistogram, yBase, widthHistogram, heightHistogram, redHistogram);
 
-    // 显示在第二行
-    // green hstgrm
-    if (greenHistogram[256] != -1)
-        drawGreenHistogram(xBase, yBase + dist + height + 1, height);
-    else
-        painter.drawText(xBase, yBase + (dist + height + 1), tr("Can't load the green component histogram."));
-    // blue hstgrm
-    if (blueHistogram[256] != -1)
-        drawBlueHistogram(xBase + dist + height, yBase + dist + height + 1, height);
-    else
-        painter.drawText(xBase + dist + height, yBase + dist + height + 1, tr("Can't load the blue component histogram."));
+    drawHistogram(xBase, yBase + dist + heightHistogram, widthHistogram, heightHistogram, greenHistogram);
+
+    drawHistogram(xBase + dist + widthHistogram, yBase + dist + heightHistogram, widthHistogram, heightHistogram, blueHistogram);
 } // paintEvent
 
-void DialogHistogram::drawGrayHistogram(int xBase, int yBase, int height)
+void DialogHistogram::drawHistogram(int xBase, int yBase, int widthHistogram, int heightHistogram, int *histogram)
 {
     qDebug().noquote() << "[Debug]" << QDateTime::currentDateTimeUtc().toString("yyyy-MM-dd hh:mm:ss.zzz") << ":"
                        << "drawGrayHistogram";
 
     QPainter painter(this);
-    painter.setPen(Qt::darkGray);
 
-    float max = grayHistogram[256];
+    float max = histogram[256];
 
-    if (max < redHistogram[256])
-        max = redHistogram[256];
+    painter.setPen(Qt::gray);
+    painter.drawRect(xBase, yBase - heightHistogram, widthHistogram, heightHistogram);
 
-    if (max < greenHistogram[256])
-        max = greenHistogram[256];
-
-    if (max < blueHistogram[256])
-        max = blueHistogram[256];
-
-    //
     for (int i = 0; i < 256; i++)
-    {
-        painter.drawLine(xBase + 1 + i, yBase, xBase + 1 + i, yBase - (float)(256.0 / max) * (float)grayHistogram[i]);
-    }
+        painter.drawLine(xBase + 1 + i, yBase, xBase + 1 + i, yBase - (float)(heightMaxLine / max) * (float)histogram[i]);
 
-    painter.drawText(xBase, yBase + 25, tr("black"));
-    painter.drawText(xBase + 220, yBase + 25, tr("white"));
-    painter.setPen(Qt::black);
-
-    painter.drawText(xBase + 40, yBase - height - 10, tr("灰度直方图"));
-
-    painter.drawText(xBase + 100, yBase + 15, tr("Intensity"));
-    painter.drawText(xBase - 84, yBase - height / 2 + 5, tr("Pixels count"));
-
-    // abscissa
-    painter.drawLine(xBase, yBase, xBase + 256 + 1, yBase);
-    painter.drawLine(xBase, yBase + 1, xBase + 256 + 1, yBase + 1);
-
-    // left ordinate
-    painter.drawLine(xBase, yBase, xBase, yBase - height);
-    painter.drawLine(xBase - 1, yBase, xBase - 1, yBase - height);
-
-    // right ordinate
-    painter.drawLine(xBase + 256 + 1, yBase, xBase + 256 + 1, yBase - height);
-    painter.drawLine(xBase + 256 + 2, yBase, xBase + 256 + 2, yBase - height);
-
-    // left ordinate arrow
-    painter.drawLine(xBase, yBase - height, xBase + 4, yBase - height + 7);
-    painter.drawLine(xBase - 1, yBase - height, xBase - 1 - 4, yBase - height + 7);
-
-    // right ordinate arrow
-    painter.drawLine(xBase + 256 + 1, yBase - height, xBase + 256 + 1 - 4, yBase - height + 7);
-    painter.drawLine(xBase + 256 + 2, yBase - height, xBase + 256 + 2 + 4, yBase - height + 7);
-} // drawGrayHistogram
-
-void DialogHistogram::drawRedHistogram(int xBase, int yBase, int height)
-{
-    qDebug().noquote() << "[Debug]" << QDateTime::currentDateTimeUtc().toString("yyyy-MM-dd hh:mm:ss.zzz") << ":"
-                       << "drawRedHistogram";
-
-    QPainter painter(this);
-
-    painter.setPen(Qt::darkRed);
-
-    float max = grayHistogram[256];
-
-    if (max < redHistogram[256])
-        max = redHistogram[256];
-
-    if (max < greenHistogram[256])
-        max = greenHistogram[256];
-
-    if (max < blueHistogram[256])
-        max = blueHistogram[256];
-
-    // drawing the histogram
-    for (int i = 0; i < 256; i++)
-    {
-        painter.drawLine(xBase + 1 + i, yBase, xBase + 1 + i,
-                         yBase - (float)(256. / max) * (float)redHistogram[i]);
-    }
-
-    painter.drawText(xBase, yBase + 25, tr("dark"));
-    painter.drawText(xBase + 225, yBase + 25, tr("light"));
-
-    painter.setPen(Qt::black);
-
-    painter.drawText(xBase + 25, yBase - height - 10, tr("RED COMPONENT HISTOGRAM"));
-
-    painter.drawText(xBase + 100, yBase + 15, tr("Intensity"));
-    painter.drawText(xBase - 84, yBase - height / 2 + 5, tr("Pixels count"));
-
-    // abscissa
-    painter.drawLine(xBase, yBase, xBase + 256 + 1, yBase);
-    painter.drawLine(xBase, yBase + 1, xBase + 256 + 1, yBase + 1);
-
-    // left ordinate
-    painter.drawLine(xBase, yBase, xBase, yBase - height);
-    painter.drawLine(xBase - 1, yBase, xBase - 1, yBase - height);
-
-    // right ordinate
-    painter.drawLine(xBase + 256 + 1, yBase, xBase + 256 + 1, yBase - height);
-    painter.drawLine(xBase + 256 + 2, yBase, xBase + 256 + 2, yBase - height);
-
-    // left ordinate arrow
-    painter.drawLine(xBase, yBase - height, xBase + 4, yBase - height + 7);
-    painter.drawLine(xBase - 1, yBase - height, xBase - 1 - 4, yBase - height + 7);
-
-    // right ordinate arrow
-    painter.drawLine(xBase + 256 + 1, yBase - height, xBase + 256 + 1 - 4, yBase - height + 7);
-    painter.drawLine(xBase + 256 + 2, yBase - height, xBase + 256 + 2 + 4, yBase - height + 7);
-} // drawRedHistogram
-
-void DialogHistogram::drawGreenHistogram(int xBase, int yBase, int height)
-{
-    qDebug().noquote() << "[Debug]" << QDateTime::currentDateTimeUtc().toString("yyyy-MM-dd hh:mm:ss.zzz") << ":"
-                       << "drawGreenHistogram";
-
-    QPainter painter(this);
-
-    painter.setPen(Qt::darkGreen);
-
-    float max = grayHistogram[256];
-
-    if (max < redHistogram[256])
-        max = redHistogram[256];
-
-    if (max < greenHistogram[256])
-        max = greenHistogram[256];
-
-    if (max < blueHistogram[256])
-        max = blueHistogram[256];
-
-    // drawing the histogram
-    for (int i = 0; i < 256; i++)
-    {
-        painter.drawLine(xBase + 1 + i, yBase, xBase + 1 + i,
-                         yBase - (float)(256. / max) * (float)greenHistogram[i]);
-    }
-
-    painter.drawText(xBase, yBase + 25, tr("dark"));
-    painter.drawText(xBase + 225, yBase + 25, tr("light"));
-
-    painter.setPen(Qt::black);
-
-    painter.drawText(xBase + 15, yBase - height - 10, tr("GREEN COMPONENT HISTOGRAM"));
-
-    painter.drawText(xBase + 100, yBase + 15, tr("Intensity"));
-    painter.drawText(xBase - 84, yBase - height / 2 + 5, tr("Pixels count"));
-
-    // abscissa
-    painter.drawLine(xBase, yBase, xBase + 256 + 1, yBase);
-    painter.drawLine(xBase, yBase + 1, xBase + 256 + 1, yBase + 1);
-
-    // left ordinate
-    painter.drawLine(xBase, yBase, xBase, yBase - height);
-    painter.drawLine(xBase - 1, yBase, xBase - 1, yBase - height);
-
-    // right ordinate
-    painter.drawLine(xBase + 256 + 1, yBase, xBase + 256 + 1, yBase - height);
-    painter.drawLine(xBase + 256 + 2, yBase, xBase + 256 + 2, yBase - height);
-
-    // left ordinate arrow
-    painter.drawLine(xBase, yBase - height, xBase + 4, yBase - height + 7);
-    painter.drawLine(xBase - 1, yBase - height, xBase - 1 - 4, yBase - height + 7);
-
-    // right ordinate arrow
-    painter.drawLine(xBase + 256 + 1, yBase - height, xBase + 256 + 1 - 4, yBase - height + 7);
-    painter.drawLine(xBase + 256 + 2, yBase - height, xBase + 256 + 2 + 4, yBase - height + 7);
-} // drawGreenHistogram
-
-void DialogHistogram::drawBlueHistogram(int xBase, int yBase, int height)
-{
-    qDebug().noquote() << "[Debug]" << QDateTime::currentDateTimeUtc().toString("yyyy-MM-dd hh:mm:ss.zzz") << ":"
-                       << "drawBleHistogram";
-
-    QPainter painter(this);
-
-    painter.setPen(Qt::darkBlue);
-
-    float max = grayHistogram[256];
-
-    if (max < redHistogram[256])
-        max = redHistogram[256];
-
-    if (max < greenHistogram[256])
-        max = greenHistogram[256];
-
-    if (max < blueHistogram[256])
-        max = blueHistogram[256];
-
-    // drawing the histogram
-    for (int i = 0; i < 256; i++)
-    {
-        painter.drawLine(xBase + 1 + i, yBase, xBase + 1 + i,
-                         yBase - (float)(256. / max) * (float)blueHistogram[i]);
-    }
-
-    painter.drawText(xBase, yBase + 25, tr("dark"));
-    painter.drawText(xBase + 225, yBase + 25, tr("light"));
-
-    painter.setPen(Qt::black);
-
-    painter.drawText(xBase + 20, yBase - height - 10, tr("BLUE COMPONENT HISTOGRAM"));
-
-    painter.drawText(xBase + 100, yBase + 15, tr("Intensity"));
-    painter.drawText(xBase - 84, yBase - height / 2 + 5, tr("Pixels count"));
-
-    // abscissa
-    painter.drawLine(xBase, yBase, xBase + 256 + 1, yBase);
-    painter.drawLine(xBase, yBase + 1, xBase + 256 + 1, yBase + 1);
-
-    // left ordinate
-    painter.drawLine(xBase, yBase, xBase, yBase - height);
-    painter.drawLine(xBase - 1, yBase, xBase - 1, yBase - height);
-
-    // right ordinate
-    painter.drawLine(xBase + 256 + 1, yBase, xBase + 256 + 1, yBase - height);
-    painter.drawLine(xBase + 256 + 2, yBase, xBase + 256 + 2, yBase - height);
-
-    // left ordinate arrow
-    painter.drawLine(xBase, yBase - height, xBase + 4, yBase - height + 7);
-    painter.drawLine(xBase - 1, yBase - height, xBase - 1 - 4, yBase - height + 7);
-
-    // right ordinate arrow
-    painter.drawLine(xBase + 256 + 1, yBase - height, xBase + 256 + 1 - 4, yBase - height + 7);
-    painter.drawLine(xBase + 256 + 2, yBase - height, xBase + 256 + 2 + 4, yBase - height + 7);
-} // drawBlueHistogram
-
-int DialogHistogram::getGrayHistogram(int index)
-{
-    qDebug().noquote() << "[Debug]" << QDateTime::currentDateTimeUtc().toString("yyyy-MM-dd hh:mm:ss.zzz") << ":"
-                       << "getGrayHistogram";
-
-    if (index >= 0 && index <= 258)
-        return grayHistogram[index];
-    else
-        return -2;
-} // getBwHistogram
-
-int DialogHistogram::getRedHistogram(int index)
-{
-    qDebug().noquote() << "[Debug]" << QDateTime::currentDateTimeUtc().toString("yyyy-MM-dd hh:mm:ss.zzz") << ":"
-                       << "getRedHistogram";
-
-    if (index >= 0 && index <= 257)
-        return redHistogram[index];
-    else
-        return -2;
-} // getRedHistogram
-
-int DialogHistogram::getGreenHistogram(int index)
-{
-    qDebug().noquote() << "[Debug]" << QDateTime::currentDateTimeUtc().toString("yyyy-MM-dd hh:mm:ss.zzz") << ":"
-                       << "getGreenHistogram";
-
-    if (index >= 0 && index <= 257)
-        return greenHistogram[index];
-    else
-        return -2;
-} // getGreenHistogram
-
-int DialogHistogram::getBlueHistogram(int index)
-{
-    qDebug().noquote() << "[Debug]" << QDateTime::currentDateTimeUtc().toString("yyyy-MM-dd hh:mm:ss.zzz") << ":"
-                       << "getBlueHistogram";
-
-    if (index >= 0 && index <= 257)
-        return blueHistogram[index];
-    else
-        return -2;
-} // getBlueHistogram
-
-// [Debug] 2019-10-01 12:56:16.425 : setup
-// [Debug] 2019-10-01 12:56:16.425 : init
-// [Debug] 2019-10-01 12:56:16.431 : displayHistogram
-// [Debug] 2019-10-01 12:56:16.490 : paintEvent
-// [Debug] 2019-10-01 12:56:16.490 : drawGrayHistogram
-// [Debug] 2019-10-01 12:56:16.491 : drawRedHistogram
-// [Debug] 2019-10-01 12:56:16.492 : drawGreenHistogram
-// [Debug] 2019-10-01 12:56:16.492 : drawBleHistogram
+} // drawHistogram
--- dialoghistogram.h
+++ dialoghistogram.h
@@ -31,6 +31,18 @@ public:
     int greenHistogram[258];
     int blueHistogram[258];
 
+    int widthHistogram = 256 + 2;                 // 直方图宽度
+    int heightMaxLine = 148;                      // 直方图中最长线的最大高度
+    int heightHistogram = heightMaxLine + 10 + 2; // 直方图高度
+
+    int dist = 30;                       // 直方图之间的距离
+    int xBase = 30;                       // 第一个直方图原点的 x 坐标
+    int yBase = 30 + heightHistogram + 2; // 第一个直方图原点的 y 坐标
+
+    int width;
+    int height;
+    int totalPixel;
+
 public:
     void setup();
     void init();
@@ -39,15 +51,7 @@ public:
 
     void paintEvent(QPaintEvent *e);
 
-    void drawGrayHistogram(int xBase, int yBase, int height);
-    void drawRedHistogram(int xBase, int yBase, int height);
-    void drawGreenHistogram(int xBase, int yBase, int height);
-    void drawBlueHistogram(int xBase, int yBase, int height);
-
-    int getGrayHistogram(int index);
-    int getRedHistogram(int index);
-    int getGreenHistogram(int index);
-    int getBlueHistogram(int index);
+    void drawHistogram(int xBase, int yBase, int width, int height, int *histogram);
 };
 
 #endif // DIALOGHISTOGRAM_H
